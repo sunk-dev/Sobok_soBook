1181f1bbb745eb532636766efcbe5ca1556024a1
seon <sunkyoung.dev@gmail.com>
2023-11-26 17:25:46 +0900

    디자인중-> 기록노트까지

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,12 @@ android {
 dependencies {
 //
     implementation("com.github.ybq:Android-SpinKit:1.4.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
