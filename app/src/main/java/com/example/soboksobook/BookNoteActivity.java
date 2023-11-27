a0d6b15a1dc2eaa6709117c7067001c2fb6dabe3
seon <sunkyoung.dev@gmail.com>
2023-11-28 07:01:29 +0900

    스크롤 성공

--- app/src/main/java/com/example/soboksobook/BookNoteActivity.java
+++ app/src/main/java/com/example/soboksobook/BookNoteActivity.java
@@ -1,15 +1,28 @@
 package com.example.soboksobook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BookNoteActivity extends AppCompatActivity {
+    Button cancelBtn, saveBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.booknote);
+        cancelBtn=(Button) findViewById(R.id.cancelBtn);
+        saveBtn=(Button) findViewById(R.id.saveBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BookNoteActivity.this, BookListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
